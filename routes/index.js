fcdcd30a659fc6d7d62c172c6ae7bed329cb6c4b
idoamram <ido.movieditor@gmail.com>
2016-09-23 03:58:41 +0300

    uploader

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,21 @@ var router = express.Router();
 var API_KEY = "ggl-105229452060183835257";
 var API_SECRET = "665ae755e37c49d1b2baa54e5b7df6a1";
 var wixmedia = require("wixmedia");
+var uploader   = wixmedia.uploader(API_KEY, API_SECRET);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/wmp/upload', function(req, res, next) {
-  var config = authconfig.authConfig(authconfig.AuthModes.WIX).apiKey(API_KEY).secretKey(API_SECRET);
-
+router.post('/wmp/upload', function(req, res, next) {
+  uploader.uploadFromFile(req.body.file, function(data) {
+    console.log("New image created:" + data.imageId);
+    res.json(data);
+  }, function(error) {
+    console.log(error);
+    return next(error);
+  });
 });
 
 module.exports = router;
