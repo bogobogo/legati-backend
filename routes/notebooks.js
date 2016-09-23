66a3027588aca3358a9064de0f8a4a3caa80f249
idoamram <ido.movieditor@gmail.com>
2016-09-23 12:53:48 +0300

    get notebook by id

--- routes/notebooks.js
+++ routes/notebooks.js
@@ -12,6 +12,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:notebookId', function(req, res, next) {
+  Notebooks.findOne({ _id : req.params.notebookId}, function(err, doc) {
+        if(err == null) {
+            res.json(doc);
+        }else {
+          res(err);
+        }
+    });
+});
+    
 router.post('/', function(req, res, next) {
   Notebooks.create(req.body, function (err, post) {
     if (err) return next(err);
